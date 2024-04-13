459730e6458c92e961ac439f8cd530973f579de6
zainab <zainabiqbal@gmail.com>
2024-04-13 21:46:55 +0500

    4 commit

--- run.bat
+++ run.bat
@@ -1,3 +1,18 @@
-pytest -s -v --html=reports\report2.html testCases/excel.py
+pytest -s -v testCases/excel.py
 
 rem pytest -s -v testCases/test_login.py
+
+@echo off
+echo Running Selenium tests...
+
+rem Activate virtual environment (if applicable)
+call venv\Scripts\activate
+
+rem Navigate to the directory containing your Python script
+cd C:\auto\april\testCases
+
+rem Run the Python script
+python excel.py
+
+rem Deactivate virtual environment (if applicable)
+deactivate
